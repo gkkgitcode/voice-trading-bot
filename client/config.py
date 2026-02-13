ec4aecf005e6f438f1c3491572b4f844f0198a8f
gkkgitcode <gkkgitcode@gmail.com>
2026-02-13 10:58:23 +0530

    Added 3 more clients

--- client/config.py
+++ client/config.py
@@ -1,9 +1,12 @@
 # config.py - Remote desktop configuration
 # List up to 10 clients
 REMOTE_SERVERS = [
-    {"id": "Client 1", "host": "192.168.29.46", "port": 5000},
-    {"id": "Client 2", "host": "192.168.29.46", "port": 5001},
-    {"id": "Client 3", "host": "192.168.29.46", "port": 5002},
+    {"id": "Client 1", "host": "192.168.1.3", "port": 5000},
+    {"id": "Client 2", "host": "192.168.1.3", "port": 5001},
+    {"id": "Client 3", "host": "192.168.1.3", "port": 5002},
+    {"id": "Client 4", "host": "192.168.1.3", "port": 5003},
+    {"id": "Client 5", "host": "192.168.1.3", "port": 5004},
+    {"id": "Client 6", "host": "192.168.1.3", "port": 5005},
     # add more up to Client 10...
 ]
 
